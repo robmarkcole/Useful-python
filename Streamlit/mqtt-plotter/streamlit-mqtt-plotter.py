232fe863d4559a2d8dbee98b76806c8477adf563
Robin Cole <robmarkcole@gmail.com>
2019-10-08 09:18:13 +0100

    Update streamlit demo

--- Streamlit/mqtt-plotter/streamlit-mqtt-plotter.py
+++ Streamlit/mqtt-plotter/streamlit-mqtt-plotter.py
@@ -3,16 +3,11 @@ import paho.mqtt.client as mqtt
 import streamlit as st
 import time
 
-from thread_safe_st import ThreadSafeSt
-
 MQTT_BROKER = 'localhost'
-
-thread_safe_st = ThreadSafeSt()
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    thread_safe_st.markdown(
-      f"Connected with result code {str(rc)} to MQTT broker on {MQTT_BROKER}")
+    st.write(f"Connected with result code {str(rc)} to MQTT broker on {MQTT_BROKER}")
 
 def on_disconnect(client, userdata,rc=0):
     print("DisConnected result code "+str(rc))
@@ -20,7 +15,7 @@ def on_disconnect(client, userdata,rc=0):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    st.write(str(msg.payload).decode())
+    st.write(msg.payload.decode())
 
 client = mqtt.Client()
 client.on_connect = on_connect
