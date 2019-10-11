70bbdc6f143be334f3e64882c2983d17096a1d79
Robin Cole <robmarkcole@gmail.com>
2019-10-11 14:35:26 +0100

    Update matplotlib_animation_2D.py

--- Streamlit/live-matplotlib/matplotlib_animation_2D.py
+++ Streamlit/live-matplotlib/matplotlib_animation_2D.py
@@ -1,10 +1,17 @@
 # https://discuss.streamlit.io/t/how-to-animate-a-line-chart/164/6
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
 import time
 
+cm_hot = plt.cm.get_cmap('hot')
+
 def get_frame():
-    return np.random.randint(0, 255, size=(100,100))
+    data = np.random.randint(0, 100, size=(80,80))
+    im = cm_hot(data)
+    im = np.uint8(im * 255)
+    return Image.fromarray(im)
 
 my_image = st.image(get_frame(), caption='Random image', width=600)
 
