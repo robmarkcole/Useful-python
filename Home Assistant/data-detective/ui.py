577ae0c729c60a1ac7f4d5ccbae937e1230df3a2
Robin Cole <robmarkcole@gmail.com>
2019-12-28 08:49:07 +0000

    update HA UI

--- Home Assistant/data-detective/ui.py
+++ Home Assistant/data-detective/ui.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
+import altair as alt
+alt.data_transformers.enable('default', max_rows=None)
 
 HA_CSV = 'ha.csv'
 
@@ -20,6 +22,25 @@ def get_binary_sensors(df):
 df = get_data()
 sensors = get_sensors(df)
 binary_sensors = get_binary_sensors(df)
-# st.write(sensors)
 
-st.dataframe(df)
+st.write(f"There are {len(sensors)} sensors and {len(binary_sensors)} binary_sensors")
+
+#st.dataframe(df)
+
+def get_sensor_data(sensor_name : str):
+    return df[df['entity_name'].isin([sensor_name])]
+
+sensor_to_plot = st.selectbox('Select a sensor to plot', sensors)
+data = get_sensor_data(sensor_to_plot)
+
+my_chart = alt.Chart(data).mark_line().encode(
+    x='last_changed',
+    y=alt.Y('state', title=data.iloc[0]['friendly_name']),
+  #  color='entity_name',
+    tooltip=['entity_name', 'state', 'last_changed'],
+).properties(
+    width=600,
+    height=300
+).interactive()
+
+st.altair_chart(my_chart, width=-1)
