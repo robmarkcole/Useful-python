bf2a5f23aba28419e368f8494ace4aa833171fe8
Robin Cole <robmarkcole@gmail.com>
2019-12-29 07:04:47 +0000

    Update ui.py

--- Home Assistant/data-detective/ui.py
+++ Home Assistant/data-detective/ui.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import altair as alt
+from typing import List
 alt.data_transformers.enable('default', max_rows=None)
 
 HA_CSV = 'ha.csv'
@@ -16,31 +17,25 @@ def get_data():
 def get_sensors(df):
     return list(df[df['domain']=='sensor']['entity_name'].unique())
 
-def get_binary_sensors(df):
-    return list(df[df['domain']=='binary_sensor']['entity_name'].unique())
+def get_sensor_data(df, sensor_names : List[str]):
+    data = df[df['entity_name'].isin(sensor_names)]
+    return data
+
+def get_sensor_chart(data):
+    my_chart = alt.Chart(data).mark_line().encode(
+        x='last_changed:T',
+        y=alt.Y('state'),
+        color='entity_name',
+        tooltip=['entity_name', 'state', 'last_changed'],
+    ).interactive()
+    return my_chart
 
 df = get_data()
 sensors = get_sensors(df)
-binary_sensors = get_binary_sensors(df)
+st.write(f"There are {len(sensors)} sensors")
 
-st.write(f"There are {len(sensors)} sensors and {len(binary_sensors)} binary_sensors")
-
-#st.dataframe(df)
-
-def get_sensor_data(sensor_name : str):
-    return df[df['entity_name'].isin([sensor_name])]
-
-sensor_to_plot = st.selectbox('Select a sensor to plot', sensors)
-data = get_sensor_data(sensor_to_plot)
-
-my_chart = alt.Chart(data).mark_line().encode(
-    x='last_changed',
-    y=alt.Y('state', title=data.iloc[0]['friendly_name']),
-  #  color='entity_name',
-    tooltip=['entity_name', 'state', 'last_changed'],
-).properties(
-    width=600,
-    height=300
-).interactive()
-
-st.altair_chart(my_chart, width=-1)
+sensor_1=st.selectbox('Select sensor 1 to plot', sensors)
+sensor_2=st.selectbox('Select sensor 2 to plot', sensors)
+data = get_sensor_data(df, [sensor_1, sensor_2])
+st.altair_chart(get_sensor_chart(data))
+st.dataframe(data)
